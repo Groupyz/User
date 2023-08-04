8f289575418c28202e9b004f3d8dff3d56e5ea47
Omer Leibo <leibo.omer13@gmail.com>
2023-08-04 14:04:17 +0300

    changed '/user' path to const variable

--- api/tests/test_crud.py
+++ api/tests/test_crud.py
@@ -2,6 +2,7 @@ import pytest
 from app import app, db
 from DB.models import User
 from DB.dbHandler import generate_id
+from routes import PATH
 
 USER_ID_ERROR_MESSAGE = 'ERROR! user id not found'
 EMAIL_ERROR_MESSAGE = 'Error! Email already exists'
@@ -38,12 +39,12 @@ def test_client():
 def test_create_users(test_client):
     data_1 = {"first_name": "Terry", "last_name": "Jeffords", "password": "10", "email": "Terry@gmail.com"}
     data_2 = {"first_name": "Charles", "last_name": "Boyle", "password": "11", "email": "Charles@gmail.com"}
-    response = test_client.post('/user', json=data_1)
+    response = test_client.post(f'/{PATH}', json=data_1)
     assert response.status_code == 201
     assert_user_details(data_1, response.get_json()['message'])
     user_id = get_user_id(response)
     print_user_to_file(test_client, user_id)
-    response = test_client.post('/user', json=data_2)
+    response = test_client.post(f'/{PATH}', json=data_2)
     assert response.status_code == 201
     assert_user_details(data_2, response.get_json()['message'])
     user_id = get_user_id(response)
@@ -54,7 +55,7 @@ def test_create_users(test_client):
 def test_get_user(test_client):
     create_test_user(email_addr="test_user_1@gmail.com")
     user = get_user_by_email("test_user_1@gmail.com")
-    response = test_client.get(f'/user/{user.id}')
+    response = test_client.get(f'/{PATH}/{user.id}')
     assert response.status_code == 200
     assert_user_details(user, response.get_json()['user'], False)
     
@@ -64,7 +65,7 @@ def test_update_user(test_client):
     create_test_user(email_addr="test_user_1@gmail.com")
     data = {"first_name": "Jake", "last_name": "Peralta", "password": "11", "email": "Jake@gmail.com"}
     user = get_user_by_email("test_user_1@gmail.com")
-    response = test_client.put(f'/user/{user.id}', json=data)
+    response = test_client.put(f'/{PATH}/{user.id}', json=data)
     assert response.status_code == 200
     assert_user_details(data, response.get_json()['message'])
     print_user_to_file(test_client, user.id)
@@ -74,7 +75,7 @@ def test_update_user(test_client):
 def test_delete_user(test_client):
     create_test_user(email_addr="test_user_2@gmail.com")
     user = get_user_by_email("test_user_2@gmail.com")
-    response = test_client.delete(f'/user/{user.id}')
+    response = test_client.delete(f'/{PATH}/{user.id}')
     assert response.status_code == 200
     assert user.id == response.get_json()['message']
 
@@ -86,7 +87,7 @@ def test_delete_user(test_client):
 def test_create_existing_user(test_client):
     create_test_user(email_addr="test_user_3@gmail.com")
     data = {"first_name": "test", "last_name": "user_2", "password": "11", "email": "test_user_3@gmail.com"}
-    response = test_client.post('/user', json=data)
+    response = test_client.post(f'/{PATH}', json=data)
     assert response.status_code == 400
     assert response.get_json()['message'] == EMAIL_ERROR_MESSAGE
 
@@ -95,7 +96,7 @@ def test_create_existing_user(test_client):
 def test_update_non_existing_user(test_client):
     data = {"first_name": "Jake", "last_name": "Peralta", "password": "11", "email": "Jake@gmail.com"}
     next_id = generate_id()
-    response = test_client.put(f'/user/{next_id}', json=data)
+    response = test_client.put(f'/{PATH}/{next_id}', json=data)
     assert response.status_code == 400
     assert response.get_json()['message'] == USER_ID_ERROR_MESSAGE
 
@@ -103,7 +104,7 @@ def test_update_non_existing_user(test_client):
 # delete user that doesn't exist in DB
 def test_delete_non_existing_user(test_client):
     next_id = generate_id()
-    response = test_client.delete(f'/user/{next_id}')
+    response = test_client.delete(f'/{PATH}/{next_id}')
     assert response.status_code == 400
     assert response.get_json()['message'] == USER_ID_ERROR_MESSAGE
 
@@ -111,7 +112,7 @@ def test_delete_non_existing_user(test_client):
 # get user that doesn't exist in DB
 def test_get_non_existing_user(test_client):
     next_id = generate_id()
-    response = test_client.get(f'/user/{next_id}')
+    response = test_client.get(f'/{PATH}/{next_id}')
     assert response.status_code == 400
     assert response.get_json()['message'] == USER_ID_ERROR_MESSAGE
 
@@ -129,7 +130,7 @@ def get_user_by_email(email_addr):
 
 # print created users to output.txt
 def print_user_to_file(test_client, id):
-    response = test_client.get(f'/user/{id}')
+    response = test_client.get(f'/{PATH}/{id}')
     data = response.get_json()
     with open('logs/output.log', 'a') as f:
         if 'Jake' not in str(data):
